27e1f09ef35ce93026a50a2504859292ef741df9
Rob Meijer <email@robmeijer.co.uk>
2021-02-20 19:38:40 +0000

    Add Symfony files

--- public/index.php
+++ public/index.php
@@ -1,47 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Coming Soon | fabricon.immediate.co.uk</title>
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-          crossorigin="anonymous">
-    <style>
-        html, body {
-            height: 100%;
-        }
+<?php
 
-        #page-content {
-            flex: 1 0 auto;
-        }
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-        #sticky-footer {
-            flex-shrink: 0;
-        }
+require dirname(__DIR__).'/vendor/autoload.php';
 
-        body {
-            background: #007bff;
-            background: linear-gradient(to right, #0062E6, #33AEFF);
-        }
-    </style>
-</head>
-<body class="d-flex flex-column">
-<div id="page-content">
-    <div class="container text-center">
-        <div class="row justify-content-center">
-            <div class="col-md-7">
-                <h1 class="font-weight-light mt-4 text-white"><strong>fabricon.immediate</strong>.co.uk</h1>
-                <p class="lead text-white-50">Coming soon ...</p>
-            </div>
-        </div>
-    </div>
-</div>
-<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-    <div class="container text-center">
-        <small>Copyright &copy; <?= date('Y') ?> Immediate Media Co.</small>
-    </div>
-</footer>
-</body>
-</html>
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
