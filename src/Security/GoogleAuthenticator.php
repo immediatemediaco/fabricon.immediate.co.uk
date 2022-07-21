86aca1719cb176cab2549f490b29f29fe205455e
Rob Meijer <rob.meijer@immediate.co.uk>
2022-07-21 15:21:56 +0100

    Update Auth to use Authenticator Manager

--- src/Security/GoogleAuthenticator.php
+++ src/Security/GoogleAuthenticator.php
@@ -3,25 +3,28 @@
 namespace App\Security;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use KnpU\OAuth2ClientBundle\Client\OAuth2ClientInterface;
-use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
+use League\OAuth2\Client\Provider\GoogleUser;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class GoogleAuthenticator extends SocialAuthenticator
+class GoogleAuthenticator extends OAuth2Authenticator implements AuthenticationEntryPointInterface
 {
     public function __construct(
         private ClientRegistry $clientRegistry,
         private RouterInterface $router,
-        private UrlGeneratorInterface $urlGenerator
-    ) {}
+        private UserProviderInterface $userProvider,
+    ) {
+    }
 
     public function supports(Request $request): bool
     {
@@ -30,42 +33,37 @@ class GoogleAuthenticator extends SocialAuthenticator
 
     public function start(Request $request, AuthenticationException $authException = null): Response
     {
-        return new RedirectResponse($this->getLoginUrl(), Response::HTTP_TEMPORARY_REDIRECT);
+        $loginUrl = $this->router->generate('app_login');
+
+        return new RedirectResponse($loginUrl, Response::HTTP_TEMPORARY_REDIRECT);
     }
 
-    public function getCredentials(Request $request)
+    public function authenticate(Request $request): Passport
     {
-        return $this->fetchAccessToken($this->getGoogleClient());
+        $client = $this->clientRegistry->getClient('google_main');
+        $accessToken = $this->fetchAccessToken($client);
+
+        return new SelfValidatingPassport(
+            new UserBadge($accessToken->getToken(), function () use ($accessToken, $client) {
+                /** @var GoogleUser $googleUser */
+                $googleUser = $client->fetchUserFromToken($accessToken);
+
+                return $this->userProvider->loadUserByIdentifier($googleUser->getEmail());
+            })
+        );
     }
 
-    public function getUser($credentials, UserProviderInterface $userProvider): ?UserInterface
-    {
-        $googleUser = $this->getGoogleClient()->fetchUserFromToken($credentials);
-
-        return $userProvider->loadUserByUsername($googleUser->getEmail());
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey): Response
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         $targetUrl = $this->router->generate('app_admin');
 
         return new RedirectResponse($targetUrl);
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         $message = strtr($exception->getMessageKey(), $exception->getMessageData());
 
         return new Response($message, Response::HTTP_FORBIDDEN);
-    }
-
-    private function getGoogleClient(): OAuth2ClientInterface
-    {
-        return $this->clientRegistry->getClient('google_main');
-    }
-
-    private function getLoginUrl(): string
-    {
-        return $this->urlGenerator->generate('app_login');
     }
 }
