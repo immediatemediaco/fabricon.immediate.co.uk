f9ab93cde25d4f81ab44e5be6301e1a57aef00dc
Rob Meijer <rob.meijer@immediate.co.uk>
2022-08-01 11:42:38 +0100

    Update EasyAdmin to the latest version

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,16 +16,21 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private AdminUrlGenerator $urlGenerator,
+    ) {
+    }
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        $scheduleUrl = $this->get(CrudUrlGenerator::class)->build()
+        $scheduleUrl = $this->urlGenerator
             ->setController(ScheduleCrudController::class)
             ->generateUrl();
 
@@ -35,6 +40,12 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()->setTitle('Fabric Conference');
+    }
+
+    public function configureCrud(): Crud
+    {
+        return parent::configureCrud()
+            ->showEntityActionsInlined();
     }
 
     public function configureActions(): Actions
@@ -61,7 +72,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Suggestions', 'fas fa-exclamation-circle', Suggestion::class);
         yield MenuItem::linkToCrud('Admins', 'fa fa-users-cog', Admin::class);
         yield MenuItem::linkToCrud('Settings', 'fa fa-gear', Settings::class)
-        ->setAction(Action::DETAIL)
-        ->setEntityId(1);
+            ->setAction(Action::DETAIL)
+            ->setEntityId(1);
     }
 }
